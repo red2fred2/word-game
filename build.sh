7bbf2d3e79327835f31656f9868f3b9c15f483af
red2fred2 <red2fred2base@live.com>
2025-02-25 02:13:29 -0600

    Switch from rollup to rolldown

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 node node_modules/gulp-cli/bin/gulp.js
 
 # Build front end code
-node node_modules/rollup/dist/bin/rollup -c
+node node_modules/rolldown/bin/cli.js -c
 
 # Make docker image
 docker buildx build -t 'red2fred2/word-game' .
---
+++ rolldown.config.js
@@ -0,0 +1,24 @@
+import copy from 'rollup-plugin-copy';
+import scss from 'rollup-plugin-scss';
+
+import { defineConfig } from 'rolldown';
+
+export default defineConfig({
+	input: './src/frontend/main.tsx',
+	output: {
+		file: './output/frontend/script.js',
+		minify: true
+	},
+	plugins: [
+		copy({
+			targets: [
+				{src: 'src/frontend/**/*.html', dest: 'output/frontend/'}
+			]
+		}),
+		scss({
+			fileName: 'style.css',
+			outputStyle: 'compressed',
+			silenceDeprecations: ['legacy-js-api']
+		})
+	]
+});
