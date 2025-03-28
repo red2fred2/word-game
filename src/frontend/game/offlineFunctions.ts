7cfe209fa71f21a342b93a5874e6a4c4cc1bdffa
red2fred2 <red2fred2base@live.com>
2025-03-28 03:48:36 -0500

    Make offline functions public Having them private would have been a problem

--- src/frontend/game/offlineFunctions.ts
+++ src/frontend/game/offlineFunctions.ts
@@ -10,7 +10,7 @@ import dictionary from './dictionary.json';
  * @remarks typescript gets mad about importing the dictionary, so this is going to
  * be commented out when not in use
  */
-function getWordlistPDF(): void {
+export function getWordlistPDF(): void {
 	let count: Map<string, number> = new Map([['A', 0], ['B', 0], ['C', 0], ['D', 0], ['E', 0], ['F', 0], ['G', 0], ['H', 0], ['I', 0], ['J', 0], ['K', 0], ['L', 0], ['M', 0], ['N', 0], ['O', 0], ['P', 0], ['Q', 0], ['R', 0], ['S', 0], ['T', 0], ['U', 0], ['V', 0], ['W', 0], ['X', 0], ['Y', 0], ['Z', 0]]);
 
 	(dictionary as string[]).forEach((word: string) => {
@@ -29,7 +29,7 @@ function getWordlistPDF(): void {
  * @param array - Array to normalize
  * @returns A normalized array
  */
-function normalizeArray(array: number[]): number[] {
+export function normalizeArray(array: number[]): number[] {
 	const sum = array.reduce((a, b) => a + b);
 	const scalar = 1 / sum;
 	return array.map(n => n * scalar);
