016eb8bd51f8e4aab2c403036cc62dd5bc27bb73
red2fred2 <red2fred2base@live.com>
2025-03-28 02:00:44 -0500

    Add solver.ts

---
+++ src/frontend/game/solver.ts
@@ -0,0 +1,14 @@
+/**
+ * Functions for automatic game solving
+ * @packageDocumentation
+ */
+
+/**
+ * Finds all possible solutions to a board
+ * @remarks This is a very expensive operation, minimize its use.
+ * @param board Game board to analyze
+ * @returns List of words included in the board
+ */
+export function solve(board: string[][]): string[] {
+	throw new Error("Not implemented");
+}
