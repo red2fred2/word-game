7e9700f664ef9c07300e83ad204c0af34e576240
red2fred2 <red2fred2base@live.com>
2025-05-02 02:31:54 -0500

    Switch find_word_score to rust

--- src/frontend/game/Game.ts
+++ src/frontend/game/Game.ts
@@ -3,11 +3,9 @@
  * @packageDocumentation
  */
 
-import wasmInit, {InitOutput, generate_letters} from 'word-game';
+import wasmInit, {find_word_score, generate_letters, InitOutput} from 'word-game';
 
 import dictionary from './dictionary.json';
-
-const LETTER_VALUES = new Map(Object.entries({A: 12, B: 55, C: 23, D: 31, E: 9, F: 89, G: 42, H: 38, I: 11, J: 641, K: 139, L: 18, M: 33, N: 14, O: 14, P: 31, Q: 594, R: 14, S: 14, T: 15, U: 27, V: 106, W: 156, X: 333, Y: 50, Z: 237}));
 
 export enum WordCheck {
 	AlreadyFound,
@@ -92,12 +90,7 @@ export class Game {
 	 * @param word - Word to score
 	 */
 	findWordScore = (word: string): number => {
-		return word.toUpperCase()
-			.split('')
-			.reduce((wordValue: number, letter: string) => {
-				const letterValue = LETTER_VALUES.get(letter)!;
-				return wordValue + letterValue
-			}, 0);
+		return find_word_score(word);
 	}
 
 	/** Gets a letter on the game board */
--- src/frontend/game/game.rs
+++ src/frontend/game/game.rs
@@ -1,5 +1,18 @@
+use log;
 use wasm_bindgen::prelude::*;
 
-use super::generation::get_letter_values;
+use super::{const_fns::u8_to_letter_index, generation::get_letter_values};
 
 const LETTER_VALUES: [u16; 26] = get_letter_values();
+
+#[wasm_bindgen]
+pub fn find_word_score(word: &str) -> u16 {
+	let mut score = 0;
+
+	for letter in word.as_bytes() {
+		let index = u8_to_letter_index(*letter);
+		score += LETTER_VALUES[index];
+	}
+
+	return score;
+}
