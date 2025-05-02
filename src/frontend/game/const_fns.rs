d93bb914e9d164a5f5866c23f87c2f46b89a9a19
red2fred2 <red2fred2base@live.com>
2025-05-02 02:14:19 -0500

    Move u8_to_letter_index into its own function This is going to be reused all over the place.

--- src/frontend/game/const_fns.rs
+++ src/frontend/game/const_fns.rs
@@ -10,17 +10,9 @@ pub const fn count_letters<const N: usize>(dictionary: &[&str; N]) -> [u32; 26] 
 		let word_length = word.len();
 
 		while j < word_length {
-			let code = word[j];
-			let index;
-			if code > 64 && code < 91 {
-				index = code - 65;
-			} else if code > 96 && code < 123 {
-				index = code - 97;
-			} else {
-				panic!();
-			}
-
-			counts[index as usize] += 1;
+			let letter = word[j];
+			let index = u8_to_letter_index(letter);
+			counts[index] += 1;
 			j += 1;
 		}
 
@@ -59,4 +51,17 @@ pub const fn sum_u32<const N: usize>(array: &[u32; N]) -> u32 {
 	}
 
 	return sum;
+}
+
+/// Finds the index for a u8 representing a letter
+/// A and a are 0, B and b are 1 ...
+/// Panics if the letter is not A-Za-z
+pub const fn u8_to_letter_index(letter: u8) -> usize {
+	if letter > 64 && letter < 91 {
+		return (letter - 65) as usize;
+	} else if letter > 96 && letter < 123 {
+		return (letter - 97) as usize;
+	} else {
+		panic!();
+	}
 }
