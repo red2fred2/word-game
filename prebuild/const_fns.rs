66885417f55ddccdfed5ad99bda3544e06f88d8a
red2fred2 <red2fred2base@live.com>
2025-05-25 19:21:58 -0500

    Allow unused functions in const_fns This reduces the erroneous warnings due to the double import

--- prebuild/const_fns.rs
+++ prebuild/const_fns.rs
@@ -1,3 +1,4 @@
+#[allow(unused)]
 /// Counts how many of each letter exists in the dictionary
 pub const fn count_letters<const N: usize>(dictionary: &[&str; N]) -> [u32; 26] {
 	let mut counts = [0; 26];
@@ -22,6 +23,7 @@ pub const fn count_letters<const N: usize>(dictionary: &[&str; N]) -> [u32; 26] 
 	return counts;
 }
 
+#[allow(unused)]
 pub const fn normalize_array<const N: usize>(array: &[u32; N]) -> [f32; N] {
 	let sum = sum_u32(array);
 	let scalar = 1.0 / (sum as f32);
@@ -40,6 +42,7 @@ pub const fn normalize_array<const N: usize>(array: &[u32; N]) -> [f32; N] {
 	return normalized;
 }
 
+#[allow(unused)]
 pub const fn sum_u32<const N: usize>(array: &[u32; N]) -> u32 {
 	let mut i = 0;
 	let mut sum = 0;
